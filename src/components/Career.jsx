e82022fcec126ca12ccab78fc11e819f29ea2063
ElyesBelgouthi <belelyesbel@gmail.com>
2024-04-04 17:47:30 +0100

    last fixing in experience section

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,42 +1,43 @@
+import { useTranslation } from "react-i18next";
+
 const CareerSection = () => {
+  const { t } = useTranslation();
+
   return (
     <section id="career-section">
       <div className="career-subsection">
-        <h1 className="career-title">Education</h1>
+        <h1 className="career-title">{t("educationSection.title")}</h1>
         <div class="career-container">
           <div class="timeline">
             <ul className="time-line-list">
               <li className="time-line-list-element">
                 <div class="timeline-content">
-                  <h3 class="date">June 2021</h3>
-                  <h2> Pioneer High School of Ariana | Ariana, Tunisia </h2>
-                  <h1> Tunisian Baccalaureate in Technical Sciences</h1>
+                  <h3 class="date">{t("educationSection.1")}</h3>
+                  <h2>{t("educationSection.2")}</h2>
+                  <h1>{t("educationSection.3")}</h1>
                   <ul>
-                    <p>Achievements:</p>
+                    <p>{t("educationSection.4")}</p>
 
                     <li>
-                      Grade: <b>19.29/20</b> (Ranked <b>8th</b> in National
-                      Examination)
+                      {t("educationSection.5")}
+                      <b>19.29/20</b>
+                      {t("educationSection.6")}
+                      <b>{t("educationSection.66")}</b>
+                      {t("educationSection.666")}
                     </li>
                   </ul>
                 </div>
               </li>
               <li className="time-line-list-element">
                 <div class="timeline-content">
-                  <h3 class="date">Expected June 2026</h3>
-                  <h2>
-                    {" "}
-                    National Institute of Applied Sciences and Technology
-                    (INSAT) | Tunis, Tunisia{" "}
-                  </h2>
-                  <h1>Bachelor of Software Engineering</h1>
+                  <h3 class="date">{t("educationSection.7")}</h3>
+                  <h2> {t("educationSection.8")} </h2>
+                  <h1>{t("educationSection.9")}</h1>
 
                   <ul>
-                    <p>Achievements: </p>
-                    <li>Second Year: Ranked 8th out of 85 students.</li>
-                    <li>
-                      Freshman Year: Achieved a remarkable 6th out of 450.
-                    </li>
+                    <p>{t("educationSection.10")}</p>
+                    <li>{t("educationSection.11")}</li>
+                    <li>{t("educationSection.12")}</li>
                   </ul>
                 </div>
               </li>
@@ -45,54 +46,34 @@ const CareerSection = () => {
         </div>
       </div>
       <div className="career-subsection">
-        <h1 className="career-title">Professional Experience</h1>
+        <h1 className="career-title">{t("professionalSection.title")}</h1>
         <div class="career-container">
           <div class="timeline">
             <ul className="time-line-list">
               <li className="time-line-list-element">
                 <div class="timeline-content">
-                  <h3 class="date"> July 2022 - August 2022</h3>
-                  <h2> Tunisie Autoroutes | Tunis, Tunisia </h2>
-                  <h1> Seasonal IT Technician</h1>
+                  <h3 class="date">{t("professionalSection.1")}</h3>
+                  <h2>{t("professionalSection.2")}</h2>
+                  <h1>{t("professionalSection.3")}</h1>
 
                   <ul>
-                    <li>
-                      Resolved Network Issues using tools like Putty, ensuring
-                      connectivity.
-                    </li>
-                    <li>
-                      Provided technical support to end-users, addressing their
-                      IT-related queries.
-                    </li>
-                    <li>
-                      Resolved motorway toll payment system related errors using
-                      MS SQL.
-                    </li>
+                    <li>{t("professionalSection.4")}</li>
+                    <li>{t("professionalSection.5")}</li>
+                    <li>{t("professionalSection.6")}</li>
                   </ul>
                 </div>
               </li>
               <li className="time-line-list-element">
                 <div class="timeline-content">
-                  <h3 class="date">July 2023 - August 2023</h3>
-                  <h2> Horizon Data | Tunis, Tunisia </h2>
-                  <h1> Intern / Full-Stack Developer</h1>
+                  <h3 class="date">{t("professionalSection.7")}</h3>
+                  <h2>{t("professionalSection.8")}</h2>
+                  <h1>{t("professionalSection.9")}</h1>
 
                   <ul>
-                    <li>
-                      Developed an ERP system for a Learning Management System
-                      (LMS).
-                    </li>
-                    <li>
-                      Utilized Angular to create a user-friendly and responsive
-                      interface.
-                    </li>{" "}
-                    <li>
-                      Implemented the back-end using NestJS to ensure API
-                      development and data management.
-                    </li>
-                    <li>
-                      Designed and maintained the database using PostgreSQL.
-                    </li>
+                    <li>{t("professionalSection.10")}</li>
+                    <li>{t("professionalSection.11")}</li>
+                    <li>{t("professionalSection.12")}</li>
+                    <li>{t("professionalSection.13")}</li>
                   </ul>
                 </div>
               </li>
